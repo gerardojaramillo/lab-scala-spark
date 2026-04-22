28a151391c7a0047a077756a609d621a780abe4c
Gerardo Jaramillo <millodev@Gerardos-MacBook-Pro.local>
2026-04-21 21:27:52 -0600

    UsingOfSparkPostgres improvement.

--- src/main/scala/lab/UsingOfSparkPostgres.scala
+++ src/main/scala/lab/UsingOfSparkPostgres.scala
@@ -1,4 +1,10 @@
-package example
+/**
+ * UsingOfSparkPostgres.scala
+ * @author
+ *   Gerardo Jaramillo
+ */
+
+package lab
 
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
@@ -8,23 +14,30 @@ object UsingOfSparkPostgres {
   def main(args: Array[String]): Unit = {
     val config =
       new SparkConf()
+        .setAppName("UsingOfSparkPostgres")
         .setMaster("local[*]")
-        .setAppName("SparkPostgres")
     val spark = SparkSession
       .builder()
       .config(config)
       .getOrCreate()
+
+    spark.sparkContext.setJobDescription("UsingOfSparkPostgres")
 
     val df = spark.read
       .format("jdbc")
       .option("url", "jdbc:postgresql:eumira-atm-db")
       .option("user", "postgres")
       .option("password", "password")
-      .option("query", "select atm_id, name from atm")
+      .option("dbtable", "(select atm_id, name from atm) as subquery")
+      .option("partitionColumn", "atm_id")
+      .option("numPartitions", 10)
+      .option("lowerBound", "1")
+      .option("upperBound", "100000")
+      .option("fetchsize", "100000")
       .load()
 
-    df.show()
     df.printSchema()
+    df.show()
     spark.stop()
     spark.close()
   }
